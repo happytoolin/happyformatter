d7713f0b265971b7fe51f0e4170bf3c87c7b3869
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-01 09:17:39 +0000

    chore: Update astro.config.mjs to include playformCompress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// import sitemap from "@astrojs/sitemap";
-// import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://happyformatter.com",
-  // integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), playformCompress()],
   output: "static",
-  adapter: cloudflare(),
 });
