5b547912e5ea68590516ee0d368b71da29abb416
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-01 09:00:07 +0000

    feat: initial astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import playformCompress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://happyformatter.com",
+  integrations: [sitemap(), tailwind(), playformCompress()],
+});
