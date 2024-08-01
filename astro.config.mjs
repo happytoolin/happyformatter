8411b79a13e4098ee76cb72f063a4d7b6da8ff5b
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-01 09:10:35 +0000

    chore: Update astro.config.mjs to output static files instead of server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+// import sitemap from "@astrojs/sitemap";
+// import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://happyformatter.com",
-  integrations: [sitemap(), tailwind()],
-  output: "server",
+  // integrations: [sitemap(), tailwind()],
+  output: "static",
   adapter: cloudflare(),
 });
