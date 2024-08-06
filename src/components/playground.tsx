319ca6ecf7715f482bcc3e1a685335b8d2c3aadd
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-06 05:06:20 +0000

    fixed styling issue for playground

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -4,16 +4,15 @@ import json from "shiki/langs/json.mjs";
 import everforestLight from "shiki/themes/everforest-light.mjs";
 
 const initialJson = `{
-  "greeting": "Welcome to quicktype!",
+  "greeting": "Welcome to HappyFormatter!",
   "instructions": [
     "Type or paste JSON here",
     "Or choose a sample above",
-    "quicktype will generate code in your",
-    "chosen language to parse the sample data"
+    "HappyFormatter will format your code"
   ]
 }`;
 
-const MiniPlayground: React.FC = () => {
+const MiniPlayground = () => {
   const [input, setInput] = useState(initialJson);
   const [output, setOutput] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
@@ -57,9 +56,9 @@ const MiniPlayground: React.FC = () => {
         transformers: [
           {
             pre(node) {
-              node.properties.className = Array.isArray(node.properties.className)
-                ? node.properties.className.concat("vp-code")
-                : ["vp-code"];
+              node.properties.class = Array.isArray(node.properties.class)
+                ? node.properties.class.concat("code")
+                : ["code"];
             },
           },
         ],
@@ -82,37 +81,31 @@ const MiniPlayground: React.FC = () => {
   }, [input, highlighter]);
 
   return (
-    <div className="language-ts vp-adaptive-theme mini-playground transition-none!" style={{ colorScheme: "inherit" }}>
+    <div className="language-ts vp-adaptive-theme mini-playground transition-none" style={{ colorScheme: "inherit" }}>
       <div className="absolute z-10 p-2 px-3 pl-5 flex gap-1 items-center left-0 top-0 right-0 border-b border-gray-200">
         <div className="i-carbon:chevron-down opacity-50" />
         <div className="flex-auto" />
         <div className="opacity-50 text-xs mx-2">Playground</div>
       </div>
-      <div className="relative mt-10 min-h-100">
+      <div className="relative mt-10 min-h-[100px]">
         <span
           ref={highlightContainerRef}
           dangerouslySetInnerHTML={{ __html: output }}
-          className="shiki pt-5 font-mono tab-4 text-transparent caret-gray-700"
-          style={{ padding: "1rem", whiteSpace: "pre-wrap", wordWrap: "break-word" }}
         />
         <textarea
           ref={textAreaRef}
           value={input}
           onChange={onInput}
           onScroll={syncScroll}
-          className="whitespace-pre overflow-auto w-full h-full font-mono bg-transparent absolute inset-0 py-5 px-6 caret-gray tab-4 z-10 text-transparent caret-gray-700 tab-4"
+          className="
+            whitespace-pre
+            overflow-auto w-full h-full font-mono bg-transparent absolute inset-0 py-5 px-6
+            text-transparent caret-gray-600 tab-4 resize-none z-10 
+          "
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="off"
           spellCheck="false"
-          style={{
-            resize: "none",
-            color: "transparent",
-            backgroundColor: "transparent",
-            padding: "1rem",
-            whiteSpace: "pre-wrap",
-            wordWrap: "break-word",
-          }}
         />
       </div>
     </div>
