e2abc2e026a559b0220e4e306b8d08bd8c1bb47b
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-10 14:56:09 +0000

    feat: used nanostores to make it work on stock astro

---
+++ src/lib/throttle.ts
@@ -0,0 +1,18 @@
+export const throttle = (func: Function, limit: number) => {
+  let lastFunc: NodeJS.Timeout;
+  let lastRan: number;
+  return (...args: any[]) => {
+    if (!lastRan) {
+      func(...args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func(...args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
