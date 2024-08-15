1ed752df583efccbe550988e6b7ae4368b49ee92
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-15 18:24:26 +0000

    feat: added navbar and improved few things

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("daisyui")],
 } satisfies Config;
 
 export default config;
