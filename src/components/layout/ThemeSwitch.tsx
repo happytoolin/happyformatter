6c2209cefd8181c31064522b2e68ed5599d3e893
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-24 12:29:47 +0000

    feat: moved some components to read

--- src/components/layout/ThemeSwitch.tsx
+++ src/components/layout/ThemeSwitch.tsx
@@ -1,13 +1,13 @@
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 const themes = [
   { name: "Light", value: "light" },
@@ -20,10 +20,10 @@ const themes = [
   { name: "GitHub Dark", value: "github-dark" },
   { name: "Material Light", value: "material-light" },
   { name: "Material Dark", value: "material-dark" },
-]
+];
 
 export function ThemeSelector() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -42,5 +42,5 @@ export function ThemeSelector() {
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
