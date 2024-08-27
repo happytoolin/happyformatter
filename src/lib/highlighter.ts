69b50337b3e61a19f88d1d9c311d4ae7cd7fd810
Spark <79936503+spa5k@users.noreply.github.com>
2024-08-27 14:39:05 +0000

    fix: fixed the phantom state issue

--- src/lib/highlighter.ts
+++ src/lib/highlighter.ts
@@ -4,32 +4,19 @@ import {
   type Highlighter,
   type LanguageInput,
   type SpecialLanguage,
-} from "shiki";
+} from "shiki/bundle-web.mjs";
 
 const highlighterCache: Map<string, Highlighter> = new Map();
-let currentLanguage: string | null = null;
-let currentTheme: string | null = null;
-
-const disposeHighlighter = () => {
-  if (currentLanguage) {
-    const highlighter = highlighterCache.get(currentLanguage);
-    if (highlighter) {
-      highlighter.dispose();
-      highlighterCache.delete(currentLanguage);
-    }
-  }
-};
 
 export const loadHighlighter = async (
   language: string,
   themes: { [key: string]: string },
-): Promise<Highlighter | null> => {
-  const defaultTheme = "everforest-light";
-  if (currentLanguage === language && currentTheme === defaultTheme) {
-    return highlighterCache.get(language) || null;
+): Promise<Highlighter> => {
+  if (highlighterCache.has(language)) {
+    return highlighterCache.get(language)!;
   }
 
-  disposeHighlighter();
+  highlighterCache.clear();
 
   try {
     const hl = await createHighlighter({
@@ -40,55 +27,40 @@ export const loadHighlighter = async (
       language as BundledLanguage | LanguageInput | SpecialLanguage,
     );
     highlighterCache.set(language, hl);
-    currentLanguage = language;
-    currentTheme = defaultTheme;
+
     return hl;
   } catch (error) {
     console.error("Error loading highlighter:", error);
-    return null;
+    throw error;
   }
 };
 
 export const highlightCode = (
   code: string,
-  hl: Highlighter | null,
+  hl: Highlighter,
   language: string,
   themes: { [key: string]: string },
   id?: string,
 ): string => {
-  if (hl) {
-    return hl.codeToHtml(code, {
-      lang: language.toString(),
-      themes,
-      transformers: [
-        {
-          pre(node) {
-            node.properties.class = Array.isArray(node.properties.class)
-              ? node.properties.class.concat("code").concat("shiki")
-              : ["code" as string, "shiki" as string];
-            node.properties.id = id ? id : "";
-          },
+  return hl.codeToHtml(code, {
+    lang: language.toString(),
+    themes,
+    transformers: [
+      {
+        pre(node) {
+          node.properties.class = Array.isArray(node.properties.class)
+            ? node.properties.class.concat("code").concat("shiki")
+            : ["code" as string, "shiki" as string];
+          node.properties.id = id ? id : "";
         },
-      ],
-    });
-  }
-  return code;
+      },
+    ],
+  });
 };
 
 export async function initializeHighlighter(
   language: string,
   themes: { [key: string]: string },
 ): Promise<Highlighter> {
-  try {
-    const highlighter = await loadHighlighter(language, themes);
-
-    if (!highlighter) {
-      throw new Error("Failed to load highlighter");
-    }
-
-    return highlighter;
-  } catch (error) {
-    console.error("Failed to load highlighter:", error);
-    throw error;
-  }
+  return loadHighlighter(language, themes);
 }
